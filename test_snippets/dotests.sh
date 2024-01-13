1bc63399e6d2322d785df45dc88c7c24fe94bf4e
Jean-François B <2589111+jfbu@users.noreply.github.com>
2024-01-13 10:43:44 +0100

    FAIL=8, PASS=7, TOTAL=15 with latexmk and ./dotests.sh clean
    I have to examine snippets 12, 19, and 20 which abort with a non tagpdf
    warning/error.

--- test_snippets/dotests.sh
+++ test_snippets/dotests.sh
@@ -9,6 +9,9 @@ starline="**********************************************************************
 
 status=0
 errorfiles=""
+nboffiles=0
+nbofgoodfiles=0
+nbofbadfiles=0
 
 # pass any argument to let the script remove auxiliary files
 # for example ./dotests.sh clean
@@ -23,12 +26,15 @@ fi
 # reside in directory while they are being manually tested
 for file in $(git ls-files | grep tex)
 do
+    ((nboffiles+=1))
     $latex $file
     if [ $? -eq 0 ]
     then
+        ((nbofgoodfiles+=1))
         echo -e "\033[32m"
         echo -e "ok pour $file\033[0m"
     else
+        ((nbofbadfiles+=1))
         echo -e "\033[1;31m"
         echo "$starline"
         echo "**** PROBLÈME AVEC $file"
@@ -50,12 +56,13 @@ if [ $status -eq 0 ]
 then
     echo -e "\033[32m==== Aucune erreur lors des compilations TeX.     ====\033[0m"
 else
-    echo -e "\033[1;31m!!!! PROBLÈMES AVEC :\033[0m"
+    echo -e "\033[1;31m!!!! PROBLÈMES AVEC $nbofbadfiles/$nboffiles TESTS:\033[0m"
     for file in $errorfiles
     do
-        echo "!!!! $file"
-        tail -n 20 $(basename $file .tex).log | head -n 10 
+        echo -e "\033[1;31m$file\033[0m"
+        tail -n 20 $(basename $file .tex).log | head -n 10
     done
+    echo -e "\033[1;31mFAIL=$nbofbadfiles, PASS=$nbofgoodfiles, TOTAL=$nboffiles\033[0m"
 fi
 
 exit $status
