b4dbed3d5b30e7c5f38aa669ec76ab177cf7e629
Jean-François B <2589111+jfbu@users.noreply.github.com>
2024-01-13 09:19:31 +0100

    Add testing architecture for snippets
    Of course problems are to be expected from snippets using \label/\ref,
    or using Koma-Script, besides genuide etoc-tagging issues.
    
    We'll trim later after first run of ./dotests.sh, adding a more explicit
    list of files which will be tested, others put in a queue for revision.
    
    	new file:   test_snippets/body.txt
    	new file:   test_snippets/dotests.sh
    	new file:   test_snippets/etoc.sty
    	new file:   test_snippets/preamble.txt
    	new file:   test_snippets/testsnippet-01.tex
    	new file:   test_snippets/testsnippet-02.tex
    	new file:   test_snippets/testsnippet-03.tex
    	new file:   test_snippets/testsnippet-04.tex
    	new file:   test_snippets/testsnippet-05.tex
    	new file:   test_snippets/testsnippet-06.tex
    	new file:   test_snippets/testsnippet-07.tex
    	new file:   test_snippets/testsnippet-08.tex
    	new file:   test_snippets/testsnippet-09.tex
    	new file:   test_snippets/testsnippet-10.tex
    	new file:   test_snippets/testsnippet-11.tex
    	new file:   test_snippets/testsnippet-12.tex
    	new file:   test_snippets/testsnippet-13.tex
    	new file:   test_snippets/testsnippet-14.tex
    	new file:   test_snippets/testsnippet-15.tex
    	new file:   test_snippets/testsnippet-16.tex
    	new file:   test_snippets/testsnippet-17.tex
    	new file:   test_snippets/testsnippet-18.tex
    	new file:   test_snippets/testsnippet-19.tex
    	new file:   test_snippets/testsnippet-20.tex
    	new file:   test_snippets/testsnippet-21.tex
    	new file:   test_snippets/testsnippet-22.tex
    	new file:   test_snippets/testsnippet-23.tex
    	new file:   test_snippets/testsnippet-24.tex
    	new file:   test_snippets/testsnippet-25.tex
    	new file:   test_snippets/testsnippet-26.tex
    	new file:   test_snippets/testsnippet.txt

---
+++ test_snippets/dotests.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# export LATEX="latexmk -pdf" for using latexmk
+latex="${LATEX:-pdflatex} -halt-on-error --interaction=batchmode "
+
+shopt -s nullglob
+
+starline="******************************************************************************"
+
+status=0
+errorfiles=""
+
+# pass any argument to let the script remove auxiliary files
+# for example ./dotests.sh clean
+if [ $# -eq 0 ]
+then
+    :
+else
+    rm -f *.{aux,toc,log,div,lof,lot,out,ps,fls,fdb_latexmk}
+fi
+
+# execute tex only on committed files, allowing new ones to
+# reside in directory while they are being manually tested
+for file in $(git ls-files | grep tex)
+do
+    $latex $file
+    if [ $? -eq 0 ]
+    then
+        echo -e "\033[32m"
+        echo -e "ok pour $file\033[0m"
+    else
+        echo -e "\033[1;31m"
+        echo "$starline"
+        echo "**** PROBLÈME AVEC $file"
+        echo -e "$starline\033[0m"
+        status=1
+        errorfiles="$errorfiles $file"
+    fi
+    echo ""
+done
+
+if [ $# -eq 0 ]
+then
+    :
+else
+    echo "N.B.: auxiliary files have all been removed before compilation"
+fi
+
+if [ $status -eq 0 ]
+then
+    echo -e "\033[32m==== Aucune erreur lors des compilations TeX.     ====\033[0m"
+else
+    echo -e "\033[1;31m!!!! PROBLÈMES AVEC :\033[0m"
+    for file in $errorfiles
+    do
+        echo "!!!! $file"
+    done
+fi
+
+exit $status
+
